d458208b1cf928a34da8d98b86ff8fe8068bf9cb
pandascrushiva <velayuthamsiva55@gmail.com>
2024-04-05 21:16:18 +0530

    feat:counter app

---
+++ count/jstask/file.js
@@ -0,0 +1,42 @@
+// let current=new Date()
+// // document.write(current,"<br/>")//full date and time
+// // document.write(current.getDate())//current date
+// // document.write(current.getMinutes())
+// // document.write(current.getHours())
+// // document.write(current.getMilliseconds())
+// // document.write(current.getFullYear())
+// // document.write(current.getMonth(),"<br/>")
+// // document.write(current.getDay())
+
+// //proper date
+// let date=current.getDate()
+// let month=current.getMonth()+1
+// let year=current.getFullYear()
+
+// document.write(`${date}/${month}/${year} <br/>`)
+
+// //to get current month
+
+// let m=current.toDateString()//Apr wed 03 2024
+// document.write(m,"<br/>")
+
+// //to get full month
+
+// let f=current.toLocaleString("default",{month:"long",weekday:"long"})
+// document.write(f,"<br/>")//April Wednesday
+
+
+//task
+// 1.print the first date of month
+let a=new Date()
+document.write(a.getDate(),a.getMonth(),a.getFullYear())//1-4-2024
+
+//digital clock 
+// let d=new Date()
+// let dhour=d.getHours()
+// let h=document.getElementById("hours")
+
+// h.innerHTML=
+
+
+
